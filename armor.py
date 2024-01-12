8e8099c16ef39789c1f160a29481117f10ff80dc
Brighton Weekes <brightonweekes22@gmail.com>
2024-01-12 08:48:04 -0700

    Version 1.0

--- armor.py
+++ armor.py
@@ -1,39 +1,45 @@
-import gc
-
 class Armor:
-    def __init__(self, set_identifier, slot, defense, damage, crit, movement, summon_capacity, ability):
-        self.set_identifier = set_identifier
-        self.slot = slot    # 0 --> helmet, 1 --> chestplate, 2 --> leggings
+    def __init__(self, name, defense, damage, crit, movement, ability):
+        self.name = name
         self.defense = defense
         self.damage = damage
         self.crit = crit
         self.movement = movement
-        self.summon_capacity = summon_capacity
         self.ability = ability     # As a string
         
 
-class ArmorSet:
-    def __init__(self)
-        
-        
-copper_set = (Armor(0, 0, 1, 0, 0, 0, 0, ''), Armor(0, 1, 2, 0, 0, 0, 0, ''), Armor(0, 2, 1, 0, 0, 0, 0, ''), [1, 0, 0, 0, 0, ''])
-
-platinum_set = (Armor(1, 0, 4, 10, 5, 0, 0, ''), Armor(1, 1, 5, 0, 0, 0, 0, ''), Armor(1, 2, 4, 0, 0, 0, 0, ''), [5, 0, 0, 0, 0, 'For each 10 defense, you gain .01 damage'])
+class CopperSet:
+    helmet = Armor('copper helmet', 1, 0, 0, 0, '')
+    chestplate = Armor('copper chestplate', 2, 0, 0, 0, '')
+    leggings = Armor('copper leggings', 1, 0, 0, 0, '')
+    set_bonus = Armor('', 1, 0, 0, 0, '')
 
 
+class PlatinumSet:
+    helmet = Armor('platinum helmet', 4, 10, 5, 0, '')
+    chestplate = Armor('platinum chestplate', 5, 0, 0, 0, '')
+    leggings = Armor('platinum leggings', 4, 0, 0, 0, '')
+    set_bonus = Armor('', 5, 0, 0, 0, 'For each 10 defense, you gain .01 damage')
+
+
+class MoltenSet:
+    helmet = Armor('molten helmet', 30, 0, 0, 0, '')
+    chestplate = Armor('molten chestplate', 0, 0, 0, 0, '')
+    leggings = Armor('motlen leggings', 1, 0, 0, 0, '')
+    set_bonus = Armor('', 8, 0, 0, 0, '')
+
+
+copper = CopperSet()
+platinum = PlatinumSet()
+molten = MoltenSet()
+
+armor_sets = {copper, platinum, molten}
 
 helmets = []
 chestplates = []
 leggings = []
 
-for obj in gc.get_objects():
-    if isinstance(obj, Armor):
-        if obj.slot == 0:
-            helmets.append(obj)
-        elif obj.slot == 1:
-            chestplates.append(obj)
-        elif obj.slot == 2:
-            leggings.append(obj)
-
-print(helmets)
-print(helmets[0])
+for set in armor_sets:
+    helmets.append(set.helmet)
+    chestplates.append(set.chestplate)
+    leggings.append(set.leggings)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import armor
-from armor import sets, leggings, chestplates, helmets
+from armor import armor_sets, leggings, chestplates, helmets
 from copy import copy
 
 
@@ -11,6 +10,7 @@ selected_leggings = leggings[0]
 optimize_for = 'max_defense'
 
 if optimize_for == 'max_defense':
+
     for helm in helmets:
         if helm.defense > selected_helmet.defense:
             selected_helmet = copy(helm)
@@ -22,6 +22,17 @@ if optimize_for == 'max_defense':
     for leg in leggings:
         if leg.defense > selected_leggings.defense:
             selected_leggings = copy(leg)
+    
+    max_defense = selected_helmet.defense + selected_chestplate.defense + selected_leggings.defense
+    for set in armor_sets:
+        set_defense = set.helmet.defense + set.chestplate.defense + set.leggings.defense + set.set_bonus.defense
+        if  set_defense >= max_defense:
+            selected_helmet = copy(set.helmet)
+            selected_chestplate = copy(set.chestplate)
+            selected_leggings = copy(set.leggings)
+
+            max_defense = copy(set_defense)
 
 
-    print(selected_helmet.defense + selected_chestplate.defense + selected_leggings.defense)
+    print(f"\n{selected_helmet.name.capitalize()}, {selected_chestplate.name}, and {selected_leggings.name} give the highest overall defense of {max_defense}\n")
+
