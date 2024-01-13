902c1bf013ace1646ecbe14ed4f177962285a9a9
Brighton Weekes <brightonweekes22@gmail.com>
2024-01-13 09:43:21 -0700

    general damage type only

--- armor.py
+++ armor.py
@@ -8,25 +8,30 @@ class Armor:
         self.ability = ability     # As a string
         
 
+# Game stages in order: Pre-Boss, Pre-Brain of Cthulhu/Eater of Worlds, Pre-Perforators/Hive Mind, Pre-Skeletron, Pre-Wall of Flesh
+        
 class CopperSet:
     helmet = Armor('copper helmet', 1, 0, 0, 0, '')
     chestplate = Armor('copper chestplate', 2, 0, 0, 0, '')
     leggings = Armor('copper leggings', 1, 0, 0, 0, '')
     set_bonus = Armor('', 1, 0, 0, 0, '')
+    stage = 'Pre-Boss'
 
 
 class PlatinumSet:
     helmet = Armor('platinum helmet', 4, 10, 5, 0, '')
-    chestplate = Armor('platinum chestplate', 5, 0, 0, 0, '')
+    chestplate = Armor('platinum chestplate', 6, 0, 0, 0, '')
     leggings = Armor('platinum leggings', 4, 0, 0, 0, '')
     set_bonus = Armor('', 5, 0, 0, 0, 'For each 10 defense, you gain .01 damage')
+    stage = 'Pre-Boss'
 
 
 class MoltenSet:
-    helmet = Armor('molten helmet', 30, 0, 0, 0, '')
-    chestplate = Armor('molten chestplate', 0, 0, 0, 0, '')
+    helmet = Armor('molten helmet', 8, 0, 0, 0, '')
+    chestplate = Armor('molten chestplate', 0, 7, 0, 0, '')
     leggings = Armor('motlen leggings', 1, 0, 0, 0, '')
     set_bonus = Armor('', 8, 0, 0, 0, '')
+    stage = 'Pre-Skeletron'
 
 
 copper = CopperSet()
--- main.py
+++ main.py
@@ -6,32 +6,66 @@ selected_helmet = helmets[0]
 selected_chestplate = chestplates[0]
 selected_leggings = leggings[0]
 
+optimization = 'defense'
+target_class = 'melee'
 
-optimize_for = 'max_defense'
-
-if optimize_for == 'max_defense':
-
-    for helm in helmets:
-        if helm.defense > selected_helmet.defense:
-            selected_helmet = copy(helm)
-
-    for chest in chestplates:
-        if chest.defense > selected_chestplate.defense:
-            selected_chestplate = copy(chest)
-
-    for leg in leggings:
-        if leg.defense > selected_leggings.defense:
-            selected_leggings = copy(leg)
-    
-    max_defense = selected_helmet.defense + selected_chestplate.defense + selected_leggings.defense
-    for set in armor_sets:
-        set_defense = set.helmet.defense + set.chestplate.defense + set.leggings.defense + set.set_bonus.defense
-        if  set_defense >= max_defense:
-            selected_helmet = copy(set.helmet)
-            selected_chestplate = copy(set.chestplate)
-            selected_leggings = copy(set.leggings)
-
-            max_defense = copy(set_defense)
+if optimization == 'defense':
+    defense_weight, damage_weight, crit_weight, movement_weight = 1, 0, 0, 0
+elif optimization == 'damage_only':
+    defense_weight, damage_weight, crit_weight, movement_weight = 0, 1, 0, 0
+elif optimization == 'movement_speed':
+    defense_weight, damage_weight, crit_weight, movement_weight = 0, 0, 1, 0
+elif optimization == 'damage_and_crit':
+    defense_weight, damage_weight, crit_weight, movement_weight = 0, 1, 1, 0
+elif optimization == 'manual':
+    defense_weight, damage_weight, crit_weight, movement_weight = 1, 1, 1, 1
 
 
-    print(f"\n{selected_helmet.name.capitalize()}, {selected_chestplate.name}, and {selected_leggings.name} give the highest overall defense of {max_defense}\n")
+
+
+for helm in helmets:
+    if helm.defense*defense_weight + helm.damage*damage_weight + helm.crit*crit_weight + helm.movement*movement_weight > (
+        selected_helmet.defense*defense_weight + selected_helmet.damage*damage_weight + selected_helmet.crit*crit_weight + 
+        selected_helmet.movement*movement_weight
+        ):
+        selected_helmet = copy(helm)
+
+for chest in chestplates:
+    if chest.defense*defense_weight + chest.damage*damage_weight + chest.crit*crit_weight + chest.movement*movement_weight > (
+        selected_chestplate.defense*defense_weight + selected_chestplate.damage*damage_weight + selected_chestplate.crit*crit_weight + 
+        selected_chestplate.movement*movement_weight
+        ):
+        selected_chestplate = copy(chest)
+
+for leg in leggings:
+    if leg.defense*defense_weight + leg.damage*damage_weight + leg.crit*crit_weight + leg.movement*movement_weight > (
+        selected_leggings.defense*defense_weight + selected_leggings.damage*damage_weight + selected_leggings.crit*crit_weight + 
+        selected_leggings.movement*movement_weight
+        ):
+        selected_leggings = copy(leg)
+
+best_score = (selected_helmet.defense*defense_weight + selected_helmet.damage*damage_weight + selected_helmet.crit*crit_weight + 
+    selected_helmet.movement*movement_weight + selected_chestplate.defense*defense_weight + selected_chestplate.damage*damage_weight + 
+    selected_chestplate.crit*crit_weight + selected_chestplate.movement*movement_weight + selected_leggings.defense*defense_weight + 
+    selected_leggings.damage*damage_weight + selected_leggings.crit*crit_weight + selected_leggings.movement*movement_weight)
+
+for set in armor_sets:
+    set_score = (set.helmet.defense*defense_weight + set.helmet.damage*damage_weight + set.helmet.crit*crit_weight + 
+        set.helmet.movement*movement_weight + set.chestplate.defense*defense_weight + set.chestplate.damage*damage_weight + 
+        set.chestplate.crit*crit_weight + set.chestplate.movement*movement_weight + set.leggings.defense*defense_weight + 
+        set.leggings.damage*damage_weight + set.leggings.crit*crit_weight + set.leggings.movement*movement_weight + 
+        set.set_bonus.defense*defense_weight + set.set_bonus.damage*damage_weight + set.set_bonus.crit*crit_weight + 
+        set.set_bonus.movement*movement_weight)
+
+    if  set_score >= best_score:
+        selected_helmet = copy(set.helmet)
+        selected_chestplate = copy(set.chestplate)
+        selected_leggings = copy(set.leggings)
+
+        best_score = copy(set_score)
+
+
+if optimization == 'defense':
+    print(f'\n{selected_helmet.name.capitalize()}, {selected_chestplate.name}, and {selected_leggings.name} give the highest defense of {selected_helmet.defense+selected_chestplate.defense+selected_leggings.defense}\n')
+elif optimization == 'manual':
+    print(f'\n{selected_helmet.name.capitalize()}, {selected_chestplate.name}, and {selected_leggings.name} give the highest score of {best_score}.\n')
